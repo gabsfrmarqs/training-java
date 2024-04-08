782b9e53adb7671883026dd65a0a0996abde74e8
Gabriel Marques <marquesgabriel03@outlook.com>
2024-04-08 15:18:13 -0300

    Functionss

--- functions.java
+++ functions.java
@@ -1,5 +1,14 @@
 public class functions {
+
+    public static void saySomething(int param){
+        System.out.println("ayo");
+        if (param == 39){
+            System.out.println("OOO EEE OOO");
+        }else{
+            System.out.println("lelelele");
+        }
+    }
     public static void main(String[] args){
-        
+        saySomething(39);
     }
 }
